f522212be360409ae076e75f0c76ea5dafe70198
Vinches21 <Vinches2193@yandex.ru>
2023-01-20 09:38:24 +0300

    Сhecking the add to cart button

---
+++ lessons/test_items.py
@@ -0,0 +1,17 @@
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_in_basket_search(browser):
+    browser.get(link)
+    browser.maximize_window()
+    button_basket = browser.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
+    time.sleep(30)
+    assert len(button_basket.text) > 0, "Проблема с кнопкой"
+
